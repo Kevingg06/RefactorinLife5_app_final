8d250751da6735058ed65bdbc5824be348167873
Marynuccy <tomas.marinucci06@gmail.com>
2024-06-04 10:54:11 -0300

    creando_activitys

--- app/src/main/java/com/example/myapplication/ui/prelogin/presenter/ActivityPreLogin.kt
+++ app/src/main/java/com/example/myapplication/ui/prelogin/presenter/ActivityPreLogin.kt
@@ -1,12 +1,13 @@
-package com.example.myapplication
+package com.example.myapplication.ui.prelogin.presenter
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.R
 
-class MainActivity : AppCompatActivity() {
+class ActivityPreLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
