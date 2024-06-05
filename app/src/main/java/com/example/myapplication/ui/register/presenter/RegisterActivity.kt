0c893122cab987249da13394aedbcd250eae5d8b
Emedel Antonio <122237219+M3fizN3rdi@users.noreply.github.com>
2024-06-05 11:10:55 -0600

    Add view Register

---
+++ app/src/main/java/com/example/myapplication/ui/register/presenter/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication.ui.register.presenter
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
