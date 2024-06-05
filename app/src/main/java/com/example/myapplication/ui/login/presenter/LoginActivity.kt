9b9788d8f80182857e9a95e8e01c920ff6796324
Marynuccy <tomas.marinucci06@gmail.com>
2024-06-05 11:51:31 -0300

    finish

--- app/src/main/java/com/example/myapplication/ui/login/presenter/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/login/presenter/LoginActivity.kt
@@ -1,21 +1,30 @@
 package com.example.myapplication.ui.login.presenter
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityLoginBinding
+import com.example.myapplication.ui.register.presenter.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.registerHere.setOnClickListener {
+            navigateToRegister()
         }
+    }
+
+    private fun navigateToRegister() {
+        val myIntent = Intent(this, RegisterActivity::class.java)
+        startActivity(myIntent)
+
     }
 }
