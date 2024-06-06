0922edf8a530d2765fb9ad32c33d3a1bc36b2661
Emedel Antonio <122237219+M3fizN3rdi@users.noreply.github.com>
2024-06-05 20:00:52 -0600

    Refactorizacion de IDs en XML activity_register y resource strings

--- app/src/main/java/com/example/myapplication/ui/register/presenter/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/register/presenter/RegisterActivity.kt
@@ -1,11 +1,7 @@
 package com.example.myapplication.ui.register.presenter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
