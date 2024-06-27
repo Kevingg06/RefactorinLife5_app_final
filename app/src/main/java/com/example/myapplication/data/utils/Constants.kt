043116b0bcb8ff562384d3019b2ef28071cf2ab3
Jhosep <jhromero.abx@gmail.com>
2024-06-27 13:18:19 -0500

    control state login

--- app/src/main/java/com/example/myapplication/data/utils/Constants.kt
+++ app/src/main/java/com/example/myapplication/data/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.myapplication.data.utils
 
 object Constants {
     const val NETWORK_ERROR = "Error de Servicio."
-    const val LOGIN_FAILED = "Logueo Fallido. Intentalo nuevamente."
+    const val LOGIN_FAILED = "Error en el usuario o contraseña!"
     const val SUPPORT_EMAIL = "centro.ayuda.rla@gmail.com"
     const val SUPPORT_EMAIL_SUBJECT = "Consulta de soporte - RLA"
 
