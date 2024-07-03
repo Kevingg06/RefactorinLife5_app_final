e2458bac2d7568ac477b7aa5745731634bc77202
Jhosep <jhromero.abx@gmail.com>
2024-07-03 10:54:21 -0500

    complete usage home api

--- app/src/main/java/com/example/myapplication/ui/home/presenter/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/home/presenter/HomeActivity.kt
@@ -2,9 +2,7 @@ package com.example.myapplication.ui.home.presenter
 
 import com.example.myapplication.ui.adapter.ProductTypesAdapter
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -83,25 +81,17 @@ class HomeActivity : AppCompatActivity() {
                 is StateProduct.SuccessProductType -> {
                     hideLoading()
                     setRecicleView(data.info)
-                    Log.v("MIO", "1111");
-                    Toast.makeText(this,"1111", Toast.LENGTH_SHORT).show()
                 }
                 is StateProduct.SuccessProducts -> {
                     hideLoading()
                     setRecyclerViewProduct(data.info)
-                    Log.v("MIO", "2222");
-                    Toast.makeText(this,"2222", Toast.LENGTH_SHORT).show()
                 }
                 is StateProduct.SuccessLastUserProduct -> {
                     hideLoading()
-                    Log.v("MIO", "3333");
-                    Toast.makeText(this,"3333", Toast.LENGTH_SHORT).show()
                 }
                 is StateProduct.SuccessDailyOffer -> {
                     hideLoading()
                     setProductDailyOffer(data.info)
-                    Log.v("MIO", "4444");
-                    Toast.makeText(this,"4444", Toast.LENGTH_SHORT).show()
                 }
                 is StateProduct.Loading -> {
                     showLoading()
--- app/src/main/java/com/example/myapplication/ui/home/presenter/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/presenter/HomeViewModel.kt
@@ -4,11 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.myapplication.data.dto.model.StateProduct
+import com.example.myapplication.data.dto.response.ProductTypesResponse
+import com.example.myapplication.data.dto.response.ProductsResponse
+import com.example.myapplication.data.dto.response.SingleProductResponse
 import com.example.myapplication.data.repository.ProductRepository
 import com.example.myapplication.data.utils.Constants
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 
 class HomeViewModel(private val repository: ProductRepository = ProductRepository()) : ViewModel() {
     private val _data = MutableLiveData<StateProduct>()
@@ -17,46 +22,78 @@ class HomeViewModel(private val repository: ProductRepository = ProductRepositor
     fun getHomeInfo() {
         CoroutineScope(Dispatchers.IO).launch {
             _data.postValue(StateProduct.Loading)
-
             val response = repository.getInfoHome()
 
             response.let { info ->
-                info.productTypes.let {
-                    if (it.isSuccessful) {
-                        _data.postValue(
-                            it.body()?.let { it1 -> StateProduct.SuccessProductType(it1) })
-                    } else {
-                        _data.postValue(StateProduct.Error(Constants.PRODUCT_TYPE_FAILED))
+                processProductTypes(info.productTypes)
+                processProducts(info.products)
+                processLastUserProduct(info.lastUserProduct)
+                processDailyOffer(info.dailyOffer)
+            }
+        }
+    }
+
+    private suspend fun processProductTypes(productTypes: Response<ProductTypesResponse>?) {
+        productTypes?.let {
+            if (it.isSuccessful) {
+                it.body()?.let { body ->
+                    withContext(Dispatchers.Main) {
+                        _data.postValue(StateProduct.SuccessProductType(body))
                     }
                 }
-
-                info.products.let {
-                    if (it.isSuccessful) {
-                        _data.postValue(it.body()?.let { it1 -> StateProduct.SuccessProducts(it1) })
-                    } else {
-                        _data.postValue(StateProduct.Error(Constants.PRODUCTS_FAILED))
-                    }
-                }
-
-                info.lastUserProduct.let {
-                    if (it.isSuccessful) {
-                        _data.postValue(
-                            it.body()?.let { it1 -> StateProduct.SuccessLastUserProduct(it1) })
-                    } else {
-                        _data.postValue(StateProduct.Error(Constants.LAST_USER_PRODUCT_FAILED))
-                    }
-                }
-
-                info.dailyOffer.let {
-                    if (it.isSuccessful) {
-                        _data.postValue(
-                            it.body()?.let { it1 -> StateProduct.SuccessDailyOffer(it1) })
-                    } else {
-                        _data.postValue(StateProduct.Error(Constants.DAILY_OFFER_FAILED))
-                    }
+            } else {
+                withContext(Dispatchers.Main) {
+                    _data.postValue(StateProduct.Error(Constants.PRODUCT_TYPE_FAILED))
                 }
             }
         }
     }
 
+    private suspend fun processProducts(products: Response<ProductsResponse>?) {
+        products?.let {
+            if (it.isSuccessful) {
+                it.body()?.let { body ->
+                    withContext(Dispatchers.Main) {
+                        _data.postValue(StateProduct.SuccessProducts(body))
+                    }
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    _data.postValue(StateProduct.Error(Constants.PRODUCTS_FAILED))
+                }
+            }
+        }
+    }
+
+    private suspend fun processLastUserProduct(lastUserProduct: Response<SingleProductResponse>?) {
+        lastUserProduct?.let {
+            if (it.isSuccessful) {
+                it.body()?.let { body ->
+                    withContext(Dispatchers.Main) {
+                        _data.postValue(StateProduct.SuccessLastUserProduct(body))
+                    }
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    _data.postValue(StateProduct.Error(Constants.LAST_USER_PRODUCT_FAILED))
+                }
+            }
+        }
+    }
+
+    private suspend fun processDailyOffer(dailyOffer: Response<SingleProductResponse>?) {
+        dailyOffer?.let {
+            if (it.isSuccessful) {
+                it.body()?.let { body ->
+                    withContext(Dispatchers.Main) {
+                        _data.postValue(StateProduct.SuccessDailyOffer(body))
+                    }
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    _data.postValue(StateProduct.Error(Constants.DAILY_OFFER_FAILED))
+                }
+            }
+        }
+    }
 }
